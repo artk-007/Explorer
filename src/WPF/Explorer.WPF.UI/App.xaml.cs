6cb78352020b38af42fb0a822229637a37483e67
Кирилл Артемов <ArtK008@outlook.com>
2021-12-24 09:31:24 +0300

    начало drag and drop

--- src/WPF/Explorer.WPF.UI/App.xaml.cs
+++ src/WPF/Explorer.WPF.UI/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 using ExplorER;
+using GongSolutions.Wpf.DragDrop;
 
 namespace Explorer.WPF.UI
 {
@@ -14,6 +17,46 @@ namespace Explorer.WPF.UI
             var mainWindow = new MainWindow();
 
             mainWindow.Show();
+        }
+    }
+
+    public class ExplorerDragDrop : IDropTarget
+    {
+        private static IDropTarget _instance;
+        public static IDropTarget Instance => _instance ??= new ExplorerDragDrop(); 
+        public void DragOver(IDropInfo dropInfo)
+        {
+            if (dropInfo.TargetCollection is ObservableCollection<FileEntityViewModel> targetCollection)
+            {
+                if (dropInfo.Data is FileEntityViewModel sourceItem)
+                {
+                    if (dropInfo.TargetItem == null && !targetCollection.Contains(sourceItem))
+                    {
+                        dropInfo.Effects = DragDropEffects.Move;
+                        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+                        dropInfo.EffectText = "Move to";
+                        dropInfo.DestinationText = "В папку";
+                    }
+
+                    if (dropInfo.TargetItem is DirectoryViewModel targetFolder && targetFolder != sourceItem)
+                    {
+                        dropInfo.Effects = DragDropEffects.Move;
+                        dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+                        dropInfo.EffectText = "Move to";
+                        dropInfo.DestinationText = "В папку2";
+                    }
+                }
+
+                if (dropInfo.Data is ICollection<object> sourceItems)
+                {
+
+                }
+            }
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            
         }
     }
 }
